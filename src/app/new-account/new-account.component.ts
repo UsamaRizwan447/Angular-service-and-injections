fef7f1348aa7ff9ee9a5a9fbadaa75d2e9999eb4
Usama Rizwan <usama@amos.ie>
2023-08-17 13:34:07 +0500

    Converted logging code into a Service. Added the service in two different components using two different dependency injection syntaxes.

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,24 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
+import { LoggingService } from '../shared/LoggingService.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  private LoggingService: LoggingService;
+  constructor() {
+    this.LoggingService =  inject(LoggingService);
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.LoggingService.logStatus(accountStatus);
   }
 }
